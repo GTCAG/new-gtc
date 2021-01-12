ae3aea7da6c5a70c380fd3e086bf7cf03f3fedd1
Mark Artishuk <markyshuk@gmail.com>
2021-01-11 20:59:56 -0800

    Added font links, and main theme settings

--- src/themes/MainTheme.ts
+++ src/themes/MainTheme.ts
@@ -1,7 +1,27 @@
 import { DefaultTheme } from "styled-components";
 
 const mainTheme: DefaultTheme = {
-  colors: {},
-  fonts: {},
+  colors: {
+    primary: {
+      default: "#292827",
+      dark: "#1E1E1D",
+      light: "#B1AEAB",
+    },
+    accent: {
+      default: "#EFE4D6",
+      dark: "#E2D0BD",
+    },
+    lights: {
+      offWhite: "#F8F2F0",
+      white: "#ffffff",
+    },
+    cta: {
+      default: "#A37351",
+    },
+  },
+  fonts: {
+    display: "font-family: 'Merriweather', serif;",
+    text: "font-family: 'Montserrat', sans-serif;",
+  },
 };
 export { mainTheme };
--- src/themes/styled.d.ts
+++ src/themes/styled.d.ts
@@ -2,7 +2,27 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
-    colors: {};
-    fonts: {};
+    colors: {
+      primary: {
+        light: string;
+        default: string;
+        dark: string;
+      };
+      cta: {
+        default: string;
+      };
+      lights: {
+        white: string;
+        offWhite: string;
+      };
+      accent: {
+        default: string;
+        dark: string;
+      };
+    };
+    fonts: {
+      display: string;
+      text: string;
+    };
   }
 }
